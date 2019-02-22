916a7b73b16ed41bb4c91cda844a10f2d66ca676
yaelsprikut <ysprikut@gmail.com>
2019-02-21 22:25:47 -0500

    unignore build

---
+++ build/precache-manifest.deb0acac3645aef2f135726a95a7c9f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "df55769221d7b43a70ff",
+    "url": "/static/js/main.df557692.chunk.js"
+  },
+  {
+    "revision": "f9378f3f0483f5be3987",
+    "url": "/static/js/1.f9378f3f.chunk.js"
+  },
+  {
+    "revision": "df55769221d7b43a70ff",
+    "url": "/static/css/main.5ac51cff.chunk.css"
+  },
+  {
+    "revision": "f9378f3f0483f5be3987",
+    "url": "/static/css/1.9c89483d.chunk.css"
+  },
+  {
+    "revision": "d2a9e81632b051b094f665c35c6d5b41",
+    "url": "/index.html"
+  }
+];
